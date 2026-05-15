c1407a3e83979ff736762f35351697032c96a9ad
alexrodas <alexrodas719@gmail.com>
2026-05-15 00:41:16 -0500

    Diseños de pantallas actualizado

--- app/src/main/java/upn/edu/pe/inventariowh/AgregarProducto.java
+++ app/src/main/java/upn/edu/pe/inventariowh/AgregarProducto.java
@@ -1,6 +1,8 @@
 package upn.edu.pe.inventariowh;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -21,6 +23,24 @@ public class AgregarProducto extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Configurar dropdown de Categoría
+        String[] categorias = {"Ropa", "Calzado", "Accesorios", "Otros"};
+        ArrayAdapter<String> adapterCategorias = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, categorias);
+        AutoCompleteTextView autoCompleteCategoria = findViewById(R.id.autoCompleteCategoria);
+        autoCompleteCategoria.setAdapter(adapterCategorias);
+
+        // Configurar dropdown de Talla
+        String[] tallas = {"S", "M", "L", "XL", "XXL"};
+        ArrayAdapter<String> adapterTallas = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, tallas);
+        AutoCompleteTextView autoCompleteTalla = findViewById(R.id.autoCompleteTalla);
+        autoCompleteTalla.setAdapter(adapterTallas);
+
+        // Configurar dropdown de Color
+        String[] colores = {"Negro", "Blanco", "Rojo", "Azul", "Verde"};
+        ArrayAdapter<String> adapterColores = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, colores);
+        AutoCompleteTextView autoCompleteColor = findViewById(R.id.autoCompleteColor);
+        autoCompleteColor.setAdapter(adapterColores);
 
         ImageButton btsalir = findViewById(R.id.btsalir);
         btsalir.setOnClickListener(v -> {
